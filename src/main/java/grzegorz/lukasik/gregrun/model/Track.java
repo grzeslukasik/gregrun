eb0f93bfeddfc238826273bf8e5c7a7045e66580
grzeslukasik <grzeslukasik@github.com>
2023-07-21 21:01:00 +0200

    track.html added

--- src/main/java/grzegorz/lukasik/gregrun/model/Track.java
+++ src/main/java/grzegorz/lukasik/gregrun/model/Track.java
@@ -4,7 +4,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,6 +19,9 @@ public class Track {
     private long id;
     private String trackName;
     private String trackDescription;
+    @NotNull(message = "Date cannot be empty")
+    @FutureOrPresent(message = "Task date is incorrect")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate trackDate;
     private List<Checkpoints> checkpointsList;
 }
