6920c48f14da6398df54b7955e9fd2c9858d820e
grzeslukasik <grzeslukasik@github.com>
2023-07-21 19:52:25 +0200

    track controller added

---
+++ src/main/java/grzegorz/lukasik/gregrun/controller/TrackController.java
@@ -0,0 +1,51 @@
+package grzegorz.lukasik.gregrun.controller;
+
+import grzegorz.lukasik.gregrun.model.Track;
+import grzegorz.lukasik.gregrun.service.TrackService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class TrackController {
+
+    private List<Track> trackList = new ArrayList<>();
+    private final TrackService trackService;
+
+    public TrackController(TrackService trackService) {
+        this.trackService = trackService;
+    }
+
+    @GetMapping("/tracks")
+    public String getTracks(Model model) {
+        Track emptyTrack = new Track();
+        model.addAttribute("track", emptyTrack);
+
+        List<Track> tracks = trackService.getAll();
+        model.addAttribute("tracks", tracks);
+        return "tracks/track";
+    }
+
+    @GetMapping("/tracks/create")
+    public String schowCreateForm(Model model){
+        model.addAttribute("track", new Track());
+        return "tracks/add-track";
+    }
+
+    @PostMapping("/tracks/{id}/checkpoints")
+    public String getCheckpoints(@PathVariable("id") long id, Model model) {
+        Track track = trackService.findTrackById(id);
+
+        if (track == null) {
+            model.addAttribute("errorMessage", "Track not found!");
+            return "error";
+        }
+        model.addAttribute("track", track);
+        return "tracks/checkpoints";
+    }
+}
