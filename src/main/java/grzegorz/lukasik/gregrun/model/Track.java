4c78ee80a29ebd3967ac374830e1675fb0b0008f
grzeslukasik <grzeslukasik@github.com>
2023-07-25 22:39:35 +0200

    entities added

--- src/main/java/grzegorz/lukasik/gregrun/model/Track.java
+++ src/main/java/grzegorz/lukasik/gregrun/model/Track.java
@@ -6,7 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
 import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
@@ -15,13 +17,21 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @EqualsAndHashCode
+@Entity
 public class Track {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotEmpty
+    @Column
     private String trackName;
+    @Column
     private String trackDescription;
     @NotNull(message = "Date cannot be empty")
     @FutureOrPresent(message = "Task date is incorrect")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Column
     private LocalDate trackDate;
+    @OneToMany(mappedBy = "track")
     private List<Checkpoints> checkpointsList;
 }
